5f8825c5b5cf45e82c2b085afed2f2340c3b7911
luke <daiagi@gmail.com>
2024-08-22 11:15:21 +0700

    derive nifMap

--- src/pcs/kzg/commitment.rs
+++ src/pcs/kzg/commitment.rs
@@ -1,5 +1,5 @@
-use ark_ec::{AffineRepr, CurveGroup};
 use ark_ec::pairing::Pairing;
+use ark_ec::{AffineRepr, CurveGroup};
 use ark_serialize::*;
 use ark_std::iter::Sum;
 use ark_std::ops::{Add, Mul, Sub};
@@ -7,13 +7,31 @@ use ark_std::vec::Vec;
 
 use crate::pcs::Commitment;
 use crate::utils::ec::small_multiexp_affine;
+use rustler::{Encoder, Decoder, Env, Term, NifResult, NifMap};
 
 /// KZG commitment to G1 represented in affine coordinates.
-#[derive(Clone, Debug, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, CanonicalSerialize, CanonicalDeserialize, NifMap)]
 pub struct KzgCommitment<E: Pairing>(pub E::G1Affine);
 
+// Implement `Encoder` for `KzgCommitment`
+// impl<E: Pairing> Encoder for KzgCommitment<E> {
+//     fn encode<'a>(&self, env: Env<'a>) -> Term<'a> {
+//         let mut bytes = Vec::new();
+//         self.0.serialize_compressed(&mut bytes).unwrap();
+//         bytes.encode(env)
+//     }
+// }
 
-impl <E: Pairing> Commitment<E::ScalarField> for KzgCommitment<E> {
+// // Implement `Decoder` for `KzgCommitment`
+// impl<'a, E: Pairing> Decoder<'a> for KzgCommitment<E> {
+//     fn decode(term: Term<'a>) -> NifResult<Self> {
+//         let bytes: Vec<u8> = term.decode()?;
+//         let affine = E::G1Affine::deserialize_compressed(&*bytes).unwrap();
+//         Ok(KzgCommitment(affine))
+//     }
+// }
+
+impl<E: Pairing> Commitment<E::ScalarField> for KzgCommitment<E> {
     fn mul(&self, by: E::ScalarField) -> KzgCommitment<E> {
         KzgCommitment(self.0.mul(by).into())
     }
@@ -42,7 +60,7 @@ impl<E: Pairing> Sub<Self> for KzgCommitment<E> {
 }
 
 impl<E: Pairing> Sum<Self> for KzgCommitment<E> {
-    fn sum<I: Iterator<Item=Self>>(iter: I) -> KzgCommitment<E> {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> KzgCommitment<E> {
         KzgCommitment(iter.map(|c| c.0.into_group()).sum::<E::G1>().into_affine())
     }
 }
